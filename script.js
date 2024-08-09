1b557523cc30f83d156c3d42402eb91302a92196
Enrique <ejes2212@gmail.com>
2024-08-09 17:05:59 -0400

    cree la base del reproductor, agregue estilos al titulo y coloque etiquetas

---
+++ script.js
@@ -0,0 +1,40 @@
+let cancion = {
+    id: "musica",
+    title: "Waterloo",
+    album: "Waterloo (deluxe edition)",
+    author: "ABBA"
+
+}
+
+
+let mifuncion = (numero) => {
+    cancion.title += numero
+    
+}
+
+mifuncion(5)
+
+
+console.log (cancion)
+console.log(axios)
+
+
+axios.get('https://leonardoapi.onrender.com/songs')
+    .then(function (response) {
+        // handle success
+        response.data.songs.map((song) => {
+            let div = document.createElement('div')
+            div.setAttribute('class', "flex hover:bg-slate-100 transition-all cursor-pointer py-3 px-1 gap-5 items-center")
+
+            div.innerHTML = `
+                <img src="${song.path.front}" alt="" class="h-16">
+                <div>
+                    <p class="font-bold">${song.title}</p>
+                    <p class="opacity-60">${song.author}</p>
+                </div>
+            `
+
+
+            document.getElementById('container').appendChild(div)
+        })
+    })
